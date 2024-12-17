b72ef0007e03c8cf4239ad5312ecaed0df2ba129
Tarie Nosworthy <mrt303xx@aol.com>
2024-12-16 21:21:21 -0500

    Added base ribbon

---
+++ ProseTools/ProseToolsRibbon.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProseTools
+{
+    public partial class ProseToolsRibbon
+    {
+        private void ProseToolsRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
